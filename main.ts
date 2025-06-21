792ec3c6f9e3e980ac12cb905e0813185537808d
toraldanielllanos <bryanfrancod99@gmail.com>
2025-06-21 13:18:28 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.showString("proyecto3")
+basic.showNumber(1000)
+basic.showString("simulacion de los latidos del corazon")
+basic.showNumber(1000)
+basic.showIcon(IconNames.SmallHeart)
+basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.SmallHeart)
+basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.SmallHeart)
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
